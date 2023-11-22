3ee0662c1dab817bff1fbb0ffff7a4752421b729
Senhao JIANG <ben659@hotmail.com>
2023-11-22 16:37:16 +0100

    test safe calculator wiht mock

--- python/tests/test_safe_calculator.py
+++ python/tests/test_safe_calculator.py
@@ -14,3 +14,14 @@ def test_divide_should_not_raise_any_error_when_authorized():
         assert result == 3
     except Exception as e:
         pytest.fail(str(e))
+
+
+def test_mock_divide_should_not_raise_any_error_when_authorized():
+    mock_authorizer = Mock()
+    mock_authorizer.authorize = Mock(return_value=True)
+    calculator = SafeCalculator(mock_authorizer)
+    try:
+        result = calculator.add(1, 2)
+        assert result == 3
+    except Exception as e:
+        pytest.fail(str(e))
