e39052c235a7ae6ec728a470c5ac22759a963aac
Senhao JIANG <ben659@hotmail.com>
2023-11-22 16:57:40 +0100

    test_mail_sender with mock

--- python/tests/test_mail_sender.py
+++ python/tests/test_mail_sender.py
@@ -1,5 +1,7 @@
 from mail_sender import MailSender, Request
 from dataclasses import dataclass
+from unittest.mock import Mock
+
 
 @dataclass
 class MockResponse:
@@ -19,19 +21,16 @@ class MockHttp():
         return self.response
 
 def test_send_v1():
-    # TODO: write a test that fails due to the bug in MailSender.send_v1
     http_client = MockHttp()
     sender = MailSender(http_client)
     user = User("Name", "Email")
     response = sender.send_v1(user, "Message")
     req = response.request
     
-    assert user.name == req.name
-    assert user.email == req.email
+    assert user.name == req.name and user.email == req.email
 
 
 def test_send_v2():
-    # TODO: write a test that fails due to the bug in MailSender.send_v2
     http_client = MockHttp(MockResponse(503))
     sender = MailSender(http_client)
     user = User("Name", "Email")
@@ -39,3 +38,27 @@ def test_send_v2():
     req = response.request
 
     assert isinstance(req, Request)
+    
+def test_mock_send_v1():
+    http_client = Mock()
+    http_client.post = Mock()
+    sender = MailSender(http_client)
+    user = User("Name", "Email")
+    response = sender.send_v1(user, "Message")
+    req = response.request
+    
+    assert user.name == req.name and user.email == req.email
+
+
+def test_mock_send_v2():
+    http_client = Mock()
+    response = Mock()
+    response.code = 503
+
+    
+    http_client.post = Mock(return_value=response)
+    sender = MailSender(http_client)
+    user = User("Name", "Email")
+    response = sender.send_v2(user, "Message")
+
+    assert http_client.post.call_count == 2
