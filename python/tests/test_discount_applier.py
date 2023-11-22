2eab023f868ecb8a6e242a30606281a9e471c486
Senhao JIANG <ben659@hotmail.com>
2023-11-22 16:38:31 +0100

    test_discount applier with mock

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -24,3 +24,23 @@ def test_apply_v2():
     discApp.apply_v2(10, users)
     for i in range(len(users)):
         assert users[i] == notifier.users_notified[i]
+        
+def test_mock_apply_v1():
+    users = ["0","1","2","3"]
+    mock_notifier = Mock()
+    mock_notifier.notify = Mock()
+    discApp = DiscountApplier(mock_notifier)
+    discApp.apply_v1(10, users)
+    assert len(users) == mock_notifier.notify.call_count
+
+
+def test_mock_apply_v2():
+    users = ["0","1","2","3"]
+    users_notified = []
+    
+    mock_notifier = Mock()
+    mock_notifier.notify = Mock(side_effect=lambda user, _:users_notified.append(user))
+    discApp = DiscountApplier(mock_notifier)
+    discApp.apply_v2(10, users)
+    for i in range(len(users)):
+        assert users[i] == users_notified[i]
